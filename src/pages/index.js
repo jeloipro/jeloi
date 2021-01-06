a36b4270ab50b58f8b02ad2ced4d134f4b5e3d7e
Sr Eloi <jeloipro@gmail.com>
2021-01-06 19:27:16 -0300

    1 commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="JELOI.COM.BR | SITE EM MANUTENÇÃO" />
+    <h1>SITE EM MANUTENÇÃO</h1>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
