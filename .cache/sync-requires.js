e361dc8723c8f78db9e762116309c955344d5cb0
Sr Eloi <jeloipro@gmail.com>
2021-02-12 15:34:51 -0300

    update3 12-02-2021

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/thor/Development/gatsby/jeloi.com.br/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/thor/Development/gatsby/jeloi.com.br/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/thor/Development/gatsby/jeloi.com.br/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/home/thor/Development/gatsby/jeloi.com.br/src/pages/page-2.js"))),
